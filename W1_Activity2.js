14e987bd76a763368bee58112e5c604eca7368e8
c-tillett <til18005@byui.edu>
2025-01-14 15:20:54 -0700

    I just want this dang thing to work

---
+++ W1_Activity2.js
@@ -0,0 +1,7 @@
+const MyApples = 10;
+const FriendApples = 2;
+let totalApples = MyApples + FriendApples;
+
+document.getElementById("MyAppleElement").textContent = MyApples;
+document.getElementById("FriendAppleElement").textContent = FriendApples;
+document.getElementById("TotalAppleElement").textContent = totalApples;
